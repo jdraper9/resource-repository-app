51785f051d539a7c56a35abeb0f43db551384dfe
jdraper9 <jamesdraper24@gmail.com>
2017-01-27 10:39:40 -0800

    debug

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,14 +10,15 @@ class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.new(favorite_params)
     respond_to do |format|
-     if @favorite.save
-       format.html { redirect_to resources_path, notice: 'Favorite was successfully created.' }
-       format.json { render :'/resources/index', status: :created, location: @favorite }
-     else
-       format.html { render :new }
-       format.json { render json: @favorite.errors, status: :unprocessable_entity }
-     end
-   end
+      if @favorite.save
+        format.html { redirect_to resources_path, notice: 'Favorite was successfully created.' }
+        format.json { render :'/resources/index', status: :created, location: @favorite }
+      else
+        format.html { render :new }
+        format.json { render json: @favorite.errors, status: :unprocessable_entity }
+      end
+    end
+ end
 
   private
   def favorite_params
